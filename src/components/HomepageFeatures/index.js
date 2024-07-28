6fc4b9829a3b4dc56747b71b7075edc50781cc08
CoderEndING <delehub@gmail.com>
2024-07-28 02:37:46 -0700

    doc

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'OpenStudyNote',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+       OpenStudyNote:Summary of personal study notes.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Embedded-GD32-Board-Template ',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+      Embedded-GD32-Board-Template:Embedded MCU development tutorial
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Embedded-RTOS-Template',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Embedded-RTOS-Template:Embedded RTOS development tutorial
       </>
     ),
   },
