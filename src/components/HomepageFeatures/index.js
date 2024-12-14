75e3284b36a7e95f18ebef1354bbced56b9ef8f8
CoderEndING <delehub@gmail.com>
2024-12-13 23:52:28 -0800

    info

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Embedded-MCU-Board-Note',
-    Svg: require('@site/static/img/embedded1.jpg').default,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
       </>
@@ -13,7 +13,7 @@ const FeatureList = [
   },
   {
     title: 'Embedded-Linux-System-Note',
-    Svg: require('@site/static/img/embedded2.jpg').default,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
       </>
@@ -21,7 +21,7 @@ const FeatureList = [
   },
   {
     title: 'Embedded-RTOS-Note',
-    Svg: require('@site/static/img/embedded3.jpg').default,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
       </>
